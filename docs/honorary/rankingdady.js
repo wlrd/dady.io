3bc3525d352e634509a5fa9de2cd1896d463adf5
Waleed Malik <spitfire5295@gmail.com>
2018-10-06 20:08:33 -0700

    honorary

---
+++ docs/honorary/rankingdady.js
@@ -0,0 +1,45 @@
+function rank(){
+    var donations = JSON.parse(data)
+    donations.sort(function(a,b){
+        return a.amount < b.amount;
+    });
+
+    addDonations(donations)
+}
+
+function addDonations(donations){
+  var rankings = document.getElementById("rankings");
+  var tbody = document.createElement("tbody");
+
+  for (let e of donations.entries()){
+    var tr = document.createElement("tr");
+
+    createRow(tr, e)
+    tbody.appendChild(tr);
+    rankings.appendChild(tbody);
+  }
+}
+
+function createRow(tr, e){
+    textInTable(tr, e[0] + 1)
+    textInTable(tr, e[1].name)
+    textInTable(tr, e[1].amount)
+    textInTable(tr, e[1].causeName, e[1].causeLink)
+}
+
+function textInTable(tr, text, causeLink){
+    var td = document.createElement("td");
+    if(causeLink){
+        var a = document.createElement('a');
+        var linkText = document.createTextNode(text);
+        a.appendChild(linkText);
+        a.title = text;
+        a.href = causeLink;
+
+        td.appendChild(a)
+    } else{
+        td.appendChild(document.createTextNode(text));
+    }
+
+    tr.appendChild(td);
+}
